4675aa4bb4269446bb2de7ae29f46f13f0317d9a
DaveEveritt <deveritt@innotts.co.uk>
2016-11-29 12:30:24 +0000

    added classname warning, removed comments

--- js/greeting.js
+++ js/greeting.js
@@ -1,15 +1,3 @@
-/*
-An "Immediately-Invoked Function Expression" (IIFE) contains
-all the code. the final brackets () ensure that it executes.
-
-This is good practice because:
-- it keeps your variables out of the global namespace
-- local references to objects decrease lookup times
-- it is a foundation for further code organisation
-
-It is also an "anonymous" function because it has/needs no name.
-*/
-
 (function(){
 
   var myName = document.getElementById("my-name"); 
@@ -21,28 +9,13 @@ It is also an "anonymous" function because it has/needs no name.
     myName.innerHTML = userName.value;
     myHeader.innerHTML = userName.value;
     myHeader.classList.add("change-me");
-    /*
-    Without setting the variables first, this would be longer:
-    */
-    // document.getElementById("my-name").innerHTML =
-    // document.getElementById("user-name").value;
-
+    //NOT myHeader.className += " otherclass";
+    
     event.preventDefault(); // disables default form submission
     return false; // prevents further "bubbling" up of event
   }
 
-  /*
-  Event Listeners keep your JavaScript out of your HTML markup -
-  most professional developers avoid (e.g.) "onclick" in HTML tags.
-  Also, you can attach multiple events to any single element.
-  */
+  // avoid JavaScript in HTML markup (e.g. no "onclick" in HTML tags)
   getName.addEventListener("submit", PerformGreeting);
 
-  /*
-  Uncomment the line below - see what happens when you click
-  in the grey area outside the input "submit" button. Why?
-  */
-  // getName.addEventListener("click", PerformGreeting);
-
 }());
-// the final brackets () make the anonymous function execute immediately
