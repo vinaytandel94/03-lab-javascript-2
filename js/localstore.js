21b37d79b392a863f253a5303bdee8c57d58ce7c
DaveEveritt <deveritt@innotts.co.uk>
2016-12-12 18:30:45 +0000

    working version with console logs

--- js/localstore.js
+++ js/localstore.js
@@ -3,14 +3,24 @@
   var myName = document.getElementById("my-name"); 
   var userName = document.getElementById("user-name"); 
   var getName = document.getElementById("get-name");
-  var myHeader = document.getElementById("my-header");
+  var name = localStorage.name;
+  console.log(`global: ${name}`);
   
+  if(name !== userName.value) {
+    myName.innerHTML = name;
+    console.log(`If: ${name}`);
+  } else {
+    localStorage.name = userName.value;
+    console.log(`Else: ${name}`);
+  }
+
   function PerformGreeting() {
-    myName.innerHTML = userName.value;
-    myHeader.innerHTML = userName.value;
-    myHeader.classList.add("change-me");
+    name = userName.value;
+    localStorage.name = name;
+    console.log(`In function: ${name}`);
     return false;
   }
+
   if (typeof event === "undefined") {
     getName.onsubmit = PerformGreeting; //Firefox
   } else {
