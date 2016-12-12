8ce408730890917b96afdc403eacb87ad7976bea
DaveEveritt <deveritt@innotts.co.uk>
2016-12-12 22:47:01 +0000

    Trapped for empty field submit, cleaner logic

--- js/localstore.js
+++ js/localstore.js
@@ -6,25 +6,36 @@
   let nameStored = localStorage.name;
   console.log(`Name on page load: ${nameStored}`);
   
-  if(nameStored !== userName.value) {
-    myName.innerHTML = nameStored;
-    console.log(`Name is new/changed: ${nameStored}`);
+  if(nameStored) {
+    // If there's a name in localStorage, use it:
+    myName.innerHTML = `again ${nameStored}`;
+    console.log(`Name stored is: ${nameStored}`);
   }
-  // else {
-  //   localStorage.name = userName.value;
-  //   console.log(`Set name to new input: ${nameStored}`);
-  // }
+  else {
+    // There's no name in localStorage:
+    myName.innerHTML = "stranger";
+    console.log(`No name stored`);
+  }
 
   function PerformGreeting() {
+    if(userName.value === "") {
+      alert("Please enter a name");
+    }
+    // Get the name the user entered:
     nameStored = userName.value;
+    // Show the name in "my-name":
+    myName.innerHTML = nameStored;
+    // Put the name into localStorage:
     localStorage.name = nameStored;
-    console.log(`Greetings: ${nameStored}`);
+    console.log(`New name stored: ${nameStored}`);
     return false;
   }
 
+  // Listens for a form submit action: 
   if (typeof event === "undefined") {
-    getName.onsubmit = PerformGreeting; //Firefox
-  } else {
+    getName.onsubmit = PerformGreeting; // for Firefox
+  }
+  else {
     getName.addEventListener("submit", PerformGreeting);
     event.preventDefault();
   }
